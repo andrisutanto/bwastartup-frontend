7ef6a6f19bff69750ebbdf51d3fc5a56ef9039c3
Andri Sutanto <me@andrisutanto.com>
2022-12-25 20:59:36 +0800

    Tailwind Config & Add Assets
    Tailwind Config & Add Assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
-module.exports = {
-  theme: {},
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+ module.exports = {
+  theme: {
+    extend: {
+      boxShadow: {
+        outline: '0 0 0 1px rgba(66, 153, 225, 0.5)',
+      },
+      colors: {
+        'orange-button': '#FF872E',
+        'green-button': '#1ABC9C',
+        'purple-hover': '#4C52F8',
+        'purple-hover-stroke': '#8286FF',
+        'purple-progress': '#3B41E3',
+      },
+      borderRadius: {
+        20: '20px',
+      },
+    },
+  },
   variants: {},
   plugins: [],
   purge: {
@@ -16,7 +32,10 @@ module.exports = {
       'layouts/**/*.vue',
       'pages/**/*.vue',
       'plugins/**/*.js',
-      'nuxt.config.js'
-    ]
-  }
+      'nuxt.config.js',
+    ],
+  },
+  fontFamily: {
+    sans: ['Poppins', 'sans-serif'],
+  },
 }
